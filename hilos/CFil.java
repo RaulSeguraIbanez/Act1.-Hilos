d6e56fd91f877f22430dd6b090d9a544ae85d51d
RaulSeguraIbanez <128618606+RaulSeguraIbanez@users.noreply.github.com>
2023-11-15 19:06:26 +0100

    a

--- hilos/CFil.java
+++ hilos/CFil.java
@@ -1,5 +1,23 @@
 package hilos;
 
 public class CFil extends Thread {
+	private int temporizador = 0;
+	private String nombre = "";
 	
+	public void setter(int temporizacion) {
+		this.temporizador = temporizacion;
+	}
+	
+	public String getNombre() {
+		String nombre = this.nombre;
+		return nombre;
+	}
+	
+	public CFil(String nombre) {
+		super(nombre);
+	}
+	
+	public void run() {
+		
+	}
 }
--- hilos/CFilPrincipal.java
+++ hilos/CFilPrincipal.java
@@ -3,5 +3,8 @@ package hilos;
 public class CFilPrincipal {
 	public static void main(String[] args) {
 		
+		CFil proceso1 = new CFil("proceso1");
+		
+		proceso1.start();
 	}
 }
